85937eec2afb4a845eb57a93ebce574313ff6e69
Ethan Han <ethan.han47@gmail.com>
2017-05-12 19:29:55 -0400

    trying to add font awesome

--- js/anchor.js
+++ js/anchor.js
@@ -1,5 +1,25 @@
-(function () {
+// Inject anchor links to headers using jQuery
+$(function () {
   'use strict';
-  anchors.options.placement = 'left';
-  anchors.add('.post-section > h2, .post-section > h3, .post-section > h4, .post-section > h5, .post-section > h6');
-})();
+
+  /* selector for headers */
+  var postHeader = '.markdown-body h1, '
+    +'.markdown-body h2, '
+    +'.markdown-body h3, '
+    +'.markdown-body h4, '
+    +'.markdown-body h5, '
+    +'.markdown-body h6';
+
+  $(postHeader).filter('[id]').each(function () {
+    var header      = $(this),
+        headerID    = header.attr('id'),
+        anchorClass = 'header-link',
+        anchorIcon  = '<i class="fa fa-link" title="Link" aria-hidden="true"></i>';
+
+    if (headerID) {
+      header.prepend($('<a />').addClass(anchorClass).attr({ 'href': '#' + headerID, 'aria-hidden': 'true' }).html(anchorIcon));
+    }
+
+    return this;
+  });
+});
