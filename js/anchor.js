81027063438ca5f14ac0a6e3b0e5ebf3e3469fcc
Ethan Han <ethan.han47@gmail.com>
2017-05-12 00:07:12 -0400

    starting to build side nav. Adding anchorjs to maybe build a table of contents in the future. TODO: need to fix anchorjs icon positioning

---
+++ js/anchor.js
@@ -0,0 +1,5 @@
+(function () {
+  'use strict';
+  anchors.options.placement = 'left';
+  anchors.add('.post-section > h2, .post-section > h3, .post-section > h4, .post-section > h5, .post-section > h6');
+})();
