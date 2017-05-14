d15a082ed9a0c0ee0f030516c554001686bd5cd1
Ethan Han <ethan.han47@gmail.com>
2017-05-14 12:33:06 -0400

    setting the default class to be CLAS201. TODO: add functionality to the course selection menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $(this).scrollTop(0);
 
-  $(".category").first().find(".category-header").addClass("active");
+  $(".category.CLAS201 .category-header").addClass("active");
 });
