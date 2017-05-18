94e35af1680f2dfd51195f141615009f2f978b48
Ethan Han <ethan.han47@gmail.com>
2017-05-17 21:22:15 -0400

    adding a cookie manager for lecture selection. TODO: implement a dynamic lecture view component. Updated gitignore

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+    return null;
+}
+
 $(document).ready(function() {
   $(this).scrollTop(0);
 
-  $(".category.CLAS201 .category-header").addClass("active");
+  $(".category[lecture='CLAS201'] .category-header").addClass("active");
+  setCookie("lecture", "CLAS201");
+
+  $(".category").each(function(index) {
+    $(this).click(function() {
+      var newLecture = $(this).attr("lecture");
+      setCookie("lecture", newLecture);
+      console.log(getCookie("lecture"));
+      $(".category .category-header").removeClass("active")
+      $(".category[lecture="+newLecture+"] .category-header").addClass("active");
+    });
+  });
 });
